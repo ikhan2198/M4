368b1c88fb5e220a586e71691e0cb12e64a9a5a2
ikhan2198 <59579713+ikhan2198@users.noreply.github.com>
2020-07-21 23:03:09 +0100

    Update Main.php

--- src/ItsMythicalYT/M4/Main.php
+++ src/ItsMythicalYT/M4/Main.php
@@ -4,4 +4,24 @@ namespace ItsMythicalYT/M4/Main;
 
 use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\plugin\M4
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\Listener;
+use pocketmine\player\PlayerJoinEvent;
+
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase Implements Listener {
+  
+  public funtion onEnable () [
+     $this->getserver()->getPluginManager()->registerEvents($this, $this);
+] 
+
+  public funtion
+    
+    
+}
