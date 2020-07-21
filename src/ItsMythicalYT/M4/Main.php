dfe2f47fb24cf2c5ed0c0a6a0fb31a9fc60e3490
ikhan2198 <59579713+ikhan2198@users.noreply.github.com>
2020-07-21 23:44:06 +0100

    Update Main.php

--- src/ItsMythicalYT/M4/Main.php
+++ src/ItsMythicalYT/M4/Main.php
@@ -1,39 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ItsMythicalYT/M4/Main;
-
-use pocketmine\Server;
-use pocketmine\Player;
-
-use pocketmine\plugin\PluginBase;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-
-use pocketmine\event\Listener;
-use pocketmine\player\PlayerJoinEvent;
-
+use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase Implements Listener {
-  
-  public funtion onEnable () [
-     $this->getserver()->getPluginManager()->registerEvents($this, $this);
-] 
+class MainClass extends PluginBase{
 
-  public funtion onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
-   
-      switch($cmd->getName ()){
-        case "test":
-                 if($sender instanceof Player) {
-                   
-                   $sender->sendMessage
-        break
-      } 
-    
-    
-     return true;
-  }
-    
-    
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "example":
+				$sender->sendMessage("Hello " . $sender->getName() . "!");
+
+				return true;
+			default:
+				return false;
+		}
+	}
 }
